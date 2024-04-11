cd07e154676e360bd403b1d474194a931375b499
Britney Nguyen <nguyen26.bn@gmail.com>
2024-04-10 22:00:59 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,5 +7,10 @@ k.loadSprite("spritesheet", "./spritesheet.png"), {
     anims: {
         // hook up names to specific animations
         "idle-down": 936,
+        "walk-down": { from: 936, to: 939, loop: true, speed: 8 },
+        "idle-side": 975,
+        "walk-side": { from: 975, to: 978, loop: true, speed: 8 },
+        "idle-up": 1014,
+        "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 },
     }
 };
