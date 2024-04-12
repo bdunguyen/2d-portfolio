6da9a27160757b399b06752be973ccb080142f0d
Britney Nguyen <nguyen26.bn@gmail.com>
2024-04-11 23:29:58 -0700

    Creating the player and the logic to display dialogue

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { scaleFactor } from "./constants";
 import { k } from "./kaboomCtx";
 
 // vite assumes everything in the public folder can be accessed
@@ -14,3 +15,56 @@ k.loadSprite("spritesheet", "./spritesheet.png"), {
         "walk-up": { from: 1038, to: 1041, loop: true, speed: 8 },
     }
 };
+
+k.loadSprite("map", "./map.png");
+
+k.setBackground(k.Color.fromHex('#311047'));
+
+k.scene("main", async () => {
+    const mapData = await (await fetch("./map.json")).json();
+    const layers = mapData.layers;
+
+    const map = k.make([
+        k.sprite("map"),
+        k.pos(0),
+        k.scale(scaleFactor)
+    ])
+
+    const player = k.make ([
+        k.sprite("spritesheet", { anim: "idle-down" }),
+        k.area({ shape: new k.Rect(k.vec2(0, 3), 10, 10) }),
+        k.body(),
+        k.anchor("center"),
+        k.pos(),
+        k.scale(scaleFactor),
+        {
+            speed: 250,
+            direction: "down",
+            isinDialogue: false,
+        },
+        "player",
+    ]);
+
+    for (const layer of layers){
+        if (layer.name == "boundaries"){
+            for (const boundary of layer.objects) {
+                map.add([
+                    k.area({
+                        shape: new k.Rect(k.vec2(0), boundary.width, boundary.height),
+                    }),
+                    k.body({ isStatic: true }),
+                    k.pos(boundary.x, boundary.y),
+                    boundary.name,
+                ]);
+                if (boundary.name) {
+                    player.onCollide(boundary.name, () => {
+                        CSSFontPaletteValuesRule.isinDialouge = true;
+                        // todo
+                    });
+                }
+            }
+        }
+    }
+});
+
+k.go("main");
