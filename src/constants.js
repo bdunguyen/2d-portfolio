541118e2e1268707debd6bac6e7e2b13f60d3aeb
Britney Nguyen <nguyen26.bn@gmail.com>
2024-04-14 02:04:06 -0700

    update: object descriptions

--- src/constants.js
+++ src/constants.js
@@ -1,19 +1,16 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  "computer": `This is my PC. I work mostly in JavaScript/TypeScript these days. I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-  Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
-  "degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-  "sofa": `That's my sofa. I like to relax here and watch YouTube. 
-  I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-  You'll learn how I built this portfolio you're currently playing through!`,
-  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-  <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+  "computer": `This is my PC. I built it myself! I spend a lot of time here working on my programming assignments and sometimes playing games. You can see my projects on my Github <a href="https://github.com/bdunguyen" target="_blank">Github</a>!`,
+  "degree": `This is my yet to be awarded CS degree. I am closing my fourth year at the University of California, Davis and I'll be taking 2 more quarters after this one! Currently, I am taking Machine-Dependent Programing and Computer Organization, Human-AI Teaming, Probability, and Real Analysis. I hope to complete a math minor by the end of my degree!`,
+  "sofa": `That's my sofa. I like to relax here and watch YouTube or Netflix. I also like studying here!`,
+  "tv": `This is my TV. My favorite movies are *Interstellar* and *Big Hero 6*.`,
   "bed": `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  "resume": `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
-  "book1": `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
-  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  "exit": `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+  "resume": `This is my desk and on it is my resume. <a href="https://docs.google.com/document/d/17xAgjDCRGkJI7BqVkzKqglEa3oXInVkVFHk5nrgPkEI/edit?usp=sharing" target="_blank">Check it out!</a>
+  Contact me at bdunguyen@ucdavis.edu if you have any interesting job opportunities!`,
+  "book1": `I own many books! I buy books faster than I can read them but my favorite on this bookshelf is *The Course of Love*.`,
+  "book2": `*Everything I Never Told You* is a drama book about family dynamics and it's on this shelf.`,
+  "book3": `I've started some books that I haven't finished yet because I get distracted with other books! *Homosapiens* was one of them that I intend to finish`,
+  "book4": `Here's where I keep my current reads! I am currently readying *Heaven*.`,
+  "exit": `If you want to exit my portfolio, just close the tab.`,
 };
