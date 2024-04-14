a3db3153baf9a1023cffbdf2b82b6c6d4e1fe173
Britney Nguyen <nguyen26.bn@gmail.com>
2024-04-14 12:07:14 -0700

    update: object description (typos & rewording)

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,11 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  "computer": `This is my PC. I built it myself! I spend a lot of time here working on my programming assignments and sometimes playing games. You can see my projects on my Github <a href="https://github.com/bdunguyen" target="_blank">Github</a>!`,
+  "computer": `This is my PC. I built it myself! I spend a lot of time here working on my programming assignments and sometimes playing games. You can see my projects on my <a href="https://github.com/bdunguyen" target="_blank">Github</a>!`,
   "degree": `This is my yet to be awarded CS degree. I am closing my fourth year at the University of California, Davis and I'll be taking 2 more quarters after this one! Currently, I am taking Machine-Dependent Programing and Computer Organization, Human-AI Teaming, Probability, and Real Analysis. I hope to complete a math minor by the end of my degree!`,
   "sofa": `That's my sofa. I like to relax here and watch YouTube or Netflix. I also like studying here!`,
   "television": `This is my TV. My favorite movies are "Interstellar" and "Big Hero 6".`,
-  "bed": `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  "bed": `This where I sleep. I like to keep a journal beside my bed that I write in before I sleep and when I wake up! It keeps me motivated and clear about my goals :)`,
   "resume": `This is my desk and on it is my resume. <a href="https://docs.google.com/document/d/17xAgjDCRGkJI7BqVkzKqglEa3oXInVkVFHk5nrgPkEI/edit?usp=sharing" target="_blank">Check it out!</a>
   Contact me at bdunguyen@ucdavis.edu if you have any interesting job opportunities!`,
   "book1": `I own many books! I buy books faster than I can read them but my favorite on this bookshelf is "The Course of Love".`,
