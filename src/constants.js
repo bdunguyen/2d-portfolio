6cafbc0ffa433820d07d5af41e64b563dfa6aeb6
Britney Nguyen <nguyen26.bn@gmail.com>
2024-04-14 02:08:23 -0700

    update: object descriptions

--- src/constants.js
+++ src/constants.js
@@ -4,13 +4,13 @@ export const dialogueData = {
   "computer": `This is my PC. I built it myself! I spend a lot of time here working on my programming assignments and sometimes playing games. You can see my projects on my Github <a href="https://github.com/bdunguyen" target="_blank">Github</a>!`,
   "degree": `This is my yet to be awarded CS degree. I am closing my fourth year at the University of California, Davis and I'll be taking 2 more quarters after this one! Currently, I am taking Machine-Dependent Programing and Computer Organization, Human-AI Teaming, Probability, and Real Analysis. I hope to complete a math minor by the end of my degree!`,
   "sofa": `That's my sofa. I like to relax here and watch YouTube or Netflix. I also like studying here!`,
-  "tv": `This is my TV. My favorite movies are *Interstellar* and *Big Hero 6*.`,
+  "tv": `This is my TV. My favorite movies are "Interstellar" and "Big Hero 6".`,
   "bed": `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
   "resume": `This is my desk and on it is my resume. <a href="https://docs.google.com/document/d/17xAgjDCRGkJI7BqVkzKqglEa3oXInVkVFHk5nrgPkEI/edit?usp=sharing" target="_blank">Check it out!</a>
   Contact me at bdunguyen@ucdavis.edu if you have any interesting job opportunities!`,
-  "book1": `I own many books! I buy books faster than I can read them but my favorite on this bookshelf is *The Course of Love*.`,
-  "book2": `*Everything I Never Told You* is a drama book about family dynamics and it's on this shelf.`,
-  "book3": `I've started some books that I haven't finished yet because I get distracted with other books! *Homosapiens* was one of them that I intend to finish`,
-  "book4": `Here's where I keep my current reads! I am currently readying *Heaven*.`,
+  "book1": `I own many books! I buy books faster than I can read them but my favorite on this bookshelf is "The Course of Love".`,
+  "book2": `"Everything I Never Told You" is a drama book about family dynamics and it's on this shelf.`,
+  "book3": `I've started some books that I haven't finished yet because I get distracted with other books! "Homosapiens" was one of them that I intend to finish`,
+  "book4": `Here's where I keep my current reads! I am currently readying "Heaven".`,
   "exit": `If you want to exit my portfolio, just close the tab.`,
 };
